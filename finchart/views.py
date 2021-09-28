0efe92d1cd930379d1df480929c935178fb55028
tegnike <o3nike.teg.14@gmail.com>
2021-09-28 23:32:21 +0200

    トップページ追加 & bootstrap導入

--- finchart/views.py
+++ finchart/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# 以下を全て追加
+from django.views import generic
+from django.views.generic import TemplateView
+
+class IndexTemplateView(TemplateView):
+    template_name = 'finchart/index.html'
