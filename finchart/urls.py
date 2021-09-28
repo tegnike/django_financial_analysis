5c56cf4b438e1877844834127b1643a5a1e0f9ed
tegnike <o3nike.teg.14@gmail.com>
2021-09-28 23:51:17 +0200

    データの追加 & トップページに表示

--- finchart/urls.py
+++ finchart/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'finchart'
 
 urlpatterns = [
-    path('', views.IndexTemplateView.as_view(), name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 ]
--- finchart/views.py
+++ finchart/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
-# 以下を全て追加
 from django.views import generic
 from django.views.generic import TemplateView
+from .models import Fstatement
 
-class IndexTemplateView(TemplateView):
+class IndexView(TemplateView):
     template_name = 'finchart/index.html'
+    def get_context_data(self, **kwargs):
+        fstatement_list = Fstatement.objects.all().order_by('company')
+        params = {
+            'fstatement_list': fstatement_list,
+        }
+        return params
