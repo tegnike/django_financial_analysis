5b3949f3c3f71aaa1c3fdffc89c9bb0ba87ade42
tegnike <o3nike.teg.14@gmail.com>
2021-09-29 22:39:50 +0200

    ページネーション追加

--- finchart/views.py
+++ finchart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.views.generic import TemplateView, DetailView
 from .models import Company, Fstatement
+from django.views.generic.list import MultipleObjectMixin
 
 class IndexView(TemplateView):
     template_name = 'finchart/index.html'
@@ -12,17 +13,15 @@ class IndexView(TemplateView):
         }
         return params
 
-class CompanyView(DetailView):
+class CompanyView(DetailView, MultipleObjectMixin):
     model = Company
+    paginate_by = 4
 
     def get_context_data(self, **kwargs):
-        company_name = kwargs['object'].name
-        fstatement_list = Fstatement.objects.filter(company=kwargs['object']).order_by('-fiscal_year')[:4]
-        params = {
-            'company_name': company_name,
-            'fstatement_list': fstatement_list,
-        }
-        return params
+        object_list = Fstatement.objects.filter(company=kwargs['object']).order_by('-fiscal_year')
+        context = super(CompanyView, self).get_context_data(object_list=object_list, **kwargs)
+        
+        return context
 
 class FstatementView(DetailView):
     model = Fstatement
