4b093f62d3489e13ab0deb436cad9636c60c4483
tegnike <o3nike.teg.14@gmail.com>
2021-09-29 20:04:24 +0200

    ヘッダーに会社詳細へのリンク作成（詳細画面は未実装）

--- finchart/views.py
+++ finchart/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from django.views import generic
-from django.views.generic import TemplateView
-from .models import Fstatement
+from django.views.generic import TemplateView, DetailView
+from .models import Company, Fstatement
 
 class IndexView(TemplateView):
     template_name = 'finchart/index.html'
     def get_context_data(self, **kwargs):
         fstatement_list = Fstatement.objects.all().order_by('company')
         params = {
+            'fstatement_list': fstatement_list,
+        }
+        return params
+
+class CompanyView(DetailView):
+    model = Company
+
+    def get_context_data(self, **kwargs):
+        company_name = kwargs['object'].name
+        fstatement_list = Fstatement.objects.filter(company=kwargs['object']).order_by('-fiscal_year')[:4]
+        params = {
+            'company_name': company_name,
             'fstatement_list': fstatement_list,
         }
         return params
